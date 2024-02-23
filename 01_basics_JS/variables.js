6e89ce4d8b12d59dc13fd1ff29c632a2d0492f65
HARSHIT KUMAR JAIN <113702557+harshitja31@users.noreply.github.com>
2024-02-23 16:35:28 +0000

    variables

---
+++ 01_basics_JS/variables.js
@@ -0,0 +1,9 @@
+const name = "Harshit Jain"; // constant value can't changed 
+let email = "jain@gmail.com"; 
+var number = 8824547308; // before 2015 issue in block & functional scope 
+town = "Mandawar"; // bad practice
+let id; // give undefined because we have not declare the value
+
+// name = "Anjali Jain";
+
+console.table([name, email, number , town, id]);
